f5a76c849c2ffd77bc9f775fe9a1ff1ce60cf7dc
decrager <aldobrahmantyo321@gmail.com>
2023-02-15 19:00:36 +0700

    V1.2.12

--- resources/views/home/pkplh/index.blade.php
+++ resources/views/home/pkplh/index.blade.php
@@ -140,12 +140,14 @@
             <?php $i = 2; ?>
             @if ($pkplh->jenis_perubahan != 'perkep1' && $pkplh->pertek[0] != null)
                 @foreach ($pkplh->pertek as $pertek)
-                    <form @if ($pertek != "pertek6") action="{{ route('pemrakarsa.pkplh.pertek', $pkplh->id) }}" @else action="{{ route('pemrakarsa.pkplh.rintek', $pkplh->id) }}" @endif method="GET">
-                        @csrf
-                        <input type="text" name="pertek" value="{{ $pertek }}" hidden>
-                        <button type="submit" class="btn btn-primary btn-block mb-2">Preview lampiran {{ integerToRoman($i) }}</button>
-                    </form>
-                    <?php $i++; ?>
+                    @if ($pertek != null)
+                        <form @if ($pertek != "pertek6") action="{{ route('pemrakarsa.pkplh.pertek', $pkplh->id) }}" @else action="{{ route('pemrakarsa.pkplh.rintek', $pkplh->id) }}" @endif method="GET">
+                            @csrf
+                            <input type="text" name="pertek" value="{{ $pertek }}" hidden>
+                            <button type="submit" class="btn btn-primary btn-block mb-2">Preview lampiran {{ integerToRoman($i) }}</button>
+                        </form>
+                        <?php $i++; ?>
+                    @endif
                 @endforeach
             @endif
             <a class="btn btn-warning btn-block @if ($pkplh->status == "Final") disabled @endif" href="{{ route('pkplh.edit', $pkplh->id) }}">Ubah Data PKPLH</a>
