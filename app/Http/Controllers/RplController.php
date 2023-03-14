8eb974c5989fae4b77f4ee57b6ae5635f3a21422
Aldo Bramantio Nugroho <aldobrahmantyo321@gmail.com>
2023-03-14 13:27:42 +0700

    V.1.2.24

--- app/Http/Controllers/RplController.php
+++ app/Http/Controllers/RplController.php
@@ -54,7 +54,7 @@ class RplController extends Controller
 
 	public function update(Request $request, $id) //Pemrakarsa
 	{
-		return $request->all();
+		// return $request->all();
         $rpl = rpl::find($id);
 
 		$rpl->jenis_dampak		        =	$request->jenis_dampak;
